a13e0ebfc115e7eff35530ce6476275c102e10cf
HuangXiuJie <huangxiujie@HuangXiuJiedeMacBook-Pro.local>
2015-03-19 17:41:26 +0800

    tableview height

--- Beauty/HomeViewController.m
+++ Beauty/HomeViewController.m
@@ -130,8 +130,10 @@
             self.productArray = array;
             
             [self.productTableView reloadData];
-
-//            self.productTableView.frame = CGRectMake(self.productTableView.frame.origin.x, self.productTableView.frame.origin.y, self.view.frame.size.width, 120.0 * array.count);
+            
+            self.productTableView.frame = CGRectMake(self.productTableView.frame.origin.x, self.productTableView.frame.origin.y, self.view.frame.size.width, 120.0 * array.count);
+            [self.productTableView sizeThatFits:CGSizeMake( self.view.frame.size.width, 120.0 * array.count)];
+//            self.productTableView.contentSize = CGSizeMake( self.view.frame.size.width, 120.0 * array.count);
 //            self.productTableView.hidden = YES;
 //            配置高度
 //            self.mainScrollView.contentSize = CGSizeMake(self.mainScrollView.contentSize.width, self.mainScrollView.contentSize.height + array.count * 120);
